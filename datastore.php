613524f114f3730f866f4adac37629cfdeabd57c
Vinodh Rajan <vinodh.vinodh@gmail.com>
2015-07-27 20:08:15 +0100

    Add license

--- datastore.php
+++ datastore.php
@@ -1,5 +1,20 @@
 <?PHP
 
+/*
+ * Copyright (C) 2015 Vinodh Rajan vinodh@virtualvinodh.com
+ *
+ * This file is a part
+ * of Avalokitam. Avalokitam is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version. This program is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General
+ * Public License for more details. You should have received a copy of the GNU
+ * Affero General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 require_once 'google-api-php-client/src/Google/autoload.php';
 require_once 'google-api-php-client/src/Google/Client.php';
 require_once 'google-api-php-client/src/Google/Client.php';
